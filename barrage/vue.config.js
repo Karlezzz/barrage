065cbcebf43066e6dc2fbdd3a4baceee2aa8e94a
karle <970587844@qq.com>
2023-07-07 14:55:02 +0800

    update

--- barrage/vue.config.js
+++ barrage/vue.config.js
@@ -4,10 +4,10 @@ const {
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  devServer:{
-    host:'192.168.137.1',
-    port:8889,
+  // devServer:{
+  //   host:'192.168.137.1',
+  //   port:8889,
     
-  },
+  // },
   
 })
