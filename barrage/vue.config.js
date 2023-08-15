6c0961ad31703497c05eb1d3d0aa9248c0f00d00
karle <970587844@qq.com>
2023-08-15 18:12:42 +0800

    handle cros

--- barrage/vue.config.js
+++ barrage/vue.config.js
@@ -9,5 +9,17 @@ module.exports = defineConfig({
   //   port:8889,
     
   // },
+  proxyTable: {
+    '/socket.io':{
+      target: 'http://127.0.0.1:3001/', // target host
+      changeOrigin: true, // needed for virtual hosted sites
+      logLevel: 'debug'
+    },
+    '/sockjs-node': {
+      target: 'http://127.0.0.1:3001',
+      ws: false,
+      changeOrigin: true
+    }
+  }
   
 })
