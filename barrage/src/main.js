24691f4f59fa1fc66a68adfa9a1445a82ec6fead
karle <970587844@qq.com>
2023-08-15 18:13:28 +0800

    change to use socket io

--- barrage/src/main.js
+++ barrage/src/main.js
@@ -5,9 +5,29 @@ import router from './router/index.js'
 import store from './store/index'
 import global_ from './static/global'
 import echarts_ from './echats/index'
+import VueSocketIO from 'vue-socket.io'
+
+import SocketIO from 'socket.io-client'
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
+Vue.use(
+  new SocketIO({
+    // 生产环境建议关闭，开发环境打开(在控制台看到socket连接和事件监听的信息)
+    debug: true, 
+    connection:'http://localhost:3001',
+    options:{
+      // 创建时是否自动连接 我们这里设定为false,在指定页面再开启
+      autoConnect: false,
+      // 路径(默认值：/socket.io/) 
+      path: "/my-app/",
+      // 目前有两种传输方式：HTTP long-polling（可简称：polling）、WebSocket
+      transports: ['polling'],
+      // 附加请求头（在服务器端的 socket.handshake.headers 对象中找到）
+      extraHeaders:{},
+    },
+  })
+)
 new Vue({
   render: h => h(App),
   router,
