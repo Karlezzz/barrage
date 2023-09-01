e44101355928ff10fda27cdbd79d25c05c0d1c9f
karle <970587844@qq.com>
2023-09-01 20:53:19 +0800

    remarks

---
+++ barrage/src/store/Barrage/index.js
@@ -0,0 +1,35 @@
+// import { reqSocketUrl } from '@/api/request'
+// const state = {
+
+// }
+// const mutations = {
+//   USERLOGIN(state, data) {
+//     state.userLogin = data
+//     localStorage.setItem("TOKEN", data.token)
+//     localStorage.setItem('ID', data.id)
+//   }
+// }
+// const actions = {
+//   // async userLogin({ commit }, data) {
+//   //   let result = await reqUserLogin(data)
+//   //   if (result.code === 200) {
+//   //     commit("USERLOGIN", result.data)
+//   //   } else {
+//   //     console.log(error);
+//   //   }
+//   // }
+//   async getSocketUrl() {
+//     let result = await reqSocketUrl()
+//     if (result.code === 200) {
+//       console.log(result.data);
+//       return result.data
+//     } else {
+//       return null
+//     }
+//   }
+// }
+
+
+// export default {
+//   namespace: true, state, mutations, actions
+// }
