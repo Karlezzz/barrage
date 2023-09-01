49abe8fffc3598b513caa313021fcd13962cf06b
karle <970587844@qq.com>
2023-09-01 20:53:44 +0800

    init axios

--- barrage/src/api/index.js
+++ barrage/src/api/index.js
@@ -2,7 +2,22 @@ import requestMock from './requestMock'
 
 
 export const reqUserLogin = (data) => requestMock({
-    url:'https://www.fastmock.site/mock/6fb036f9a523faa1abf35ad394f8483e/api/userLogin',
-    method:'post',
-    data
+  url: 'https://www.fastmock.site/mock/6fb036f9a523faa1abf35ad394f8483e/api/userLogin',
+  method: 'post',
+  data
 })
+
+
+import axios from 'axios';
+
+const requests = axios.create({
+  baseURL: "http://localhost:3000",
+  timeout: 10000
+})
+requests.interceptors.response.use(config => {
+  return config;
+})
+requests.interceptors.request.use(config => {
+  return config;
+})
+export default requests;
