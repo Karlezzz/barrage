3c76846f324be16b9185464d1c1a02c0ab20a25e
karle <970587844@qq.com>
2023-09-18 17:21:37 +0800

    format

--- barrage/src/static/global.js
+++ barrage/src/static/global.js
@@ -1,45 +1,45 @@
 let historyBG = String(localStorage.getItem("BG"))
-let historyIsSun = localStorage.getItem('ISSUN')==='true'?true:false
-let historyIsCleanBG = localStorage.getItem('ISCLEANBG')==='true'?true:false
+let historyIsSun = localStorage.getItem('ISSUN') === 'true' ? true : false
+let historyIsCleanBG = localStorage.getItem('ISCLEANBG') === 'true' ? true : false
 
 
-let isSun = historyIsSun!=null ? historyIsSun : true
-let isCleanBG = historyIsCleanBG!=null ? historyIsCleanBG : false
+let isSun = historyIsSun != null ? historyIsSun : true
+let isCleanBG = historyIsCleanBG != null ? historyIsCleanBG : false
 let staticBG = historyBG ? historyBG : 'linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)'
 
 
 function getIsSun() {
-    return this.isSun
+  return this.isSun
 }
 
 function setIsSun(value) {
-    this.isSun = !this.isSun
+  this.isSun = !this.isSun
 }
 
 function getIsCleanBG() {
-    return this.isCleanBG
+  return this.isCleanBG
 }
 
 function setIsCleanBG(value) {
-    this.isCleanBG = !this.isCleanBG
+  this.isCleanBG = !this.isCleanBG
 }
 
 function getStaticBG() {
-    return this.staticBG
+  return this.staticBG
 }
 
 function setStaticBG(value) {
-    this.staticBG = value
+  this.staticBG = value
 }
 
 export default {
-    isSun,
-    getIsSun,
-    setIsSun,
-    isCleanBG,
-    getIsCleanBG,
-    setIsCleanBG,
-    staticBG,
-    getStaticBG,
-    setStaticBG
+  isSun,
+  getIsSun,
+  setIsSun,
+  isCleanBG,
+  getIsCleanBG,
+  setIsCleanBG,
+  staticBG,
+  getStaticBG,
+  setStaticBG
 }
